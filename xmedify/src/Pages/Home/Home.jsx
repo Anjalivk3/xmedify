da66f9e376f973d7b91eb34b7ea09dd15f698f71
Anjali jain <rohini1jb@gmail.com>
2025-09-10 06:56:20 +0530

    Some more static sections added

--- xmedify/src/Pages/Home/Home.jsx
+++ xmedify/src/Pages/Home/Home.jsx
@@ -2,15 +2,28 @@ import Blogs from "../../Components/Sections/Blogs/Blogs";
 import { Box, Grid } from "@mui/material";
 import OurFamilies from "../../Components/Sections/OurFamilies/OurFamilies";
 import PatientCaring from "../../Components/Sections/PatientCaring/PatientCaring";
+import Specialists from "../../Components/Sections/Specialists/Specialists";
+import Specialization from "../../Components/Sections/Specialization/Specialization";
+import FAQs from "../../Components/Sections/FAQs/FAQs";
+import Offers from "../../Components/Sections/Offers/Offers";
+import HeroSlider from "../../Components/HeroSlider/HeroSlider";
+import Navbar from "../../Components/NavBar/NavBar";
 
 import React from 'react'
 
 const Home = () => {
   return (
     <Box>
+      <Navbar />
+      <HeroSlider />
+      <Offers />
+      <Specialization />
+      <Specialists />
       <PatientCaring />
       <Blogs />
       <OurFamilies />
+      <FAQs />
+      
     </Box>
   )
 }
--- xmedify/src/index.js
+++ xmedify/src/index.js
@@ -135,6 +135,8 @@ mytheme.typography.h3 = {
 };
 
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
